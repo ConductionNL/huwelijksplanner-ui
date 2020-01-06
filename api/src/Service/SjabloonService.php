38338031145619833551c52b86d409a306710a58
Ruben van der Linde <ruben@conduction.nl>
2020-01-06 08:40:43 +0100

    switch to wrc

--- api/src/Service/SjabloonService.php
+++ api/src/Service/SjabloonService.php
@@ -20,88 +20,19 @@ class SjabloonService
 		
 		$this->client= new Client([
 				// Base URI is used with relative requests
-				'base_uri' => 'http://resources.demo.zaakonline.nl/',
+				'base_uri' => 'http://wrc.zaakonline.nl/applications/536bfb73-63a5-4719-b535-d835607b88b2/',
 				// You can set any number of default request options.
 				'timeout'  => 4000.0,
 		]);
 	}	
 	
-	public function getAll()
-	{
-		$response = $this->client->request('GET');
-		$response = json_decode($response->getBody()->getContents(), true);
-		return $response["hydra:member"];
-	}
-	
-	public function getPaginas()
-	{
-		$response =  $this->client->request('GET', '/sjablonen', [
-				'query' => ['type' => 'pagina']
-		]);
-		
-		$response = json_decode($response->getBody()->getContents(), true);
-		
-		return $response["hydra:member"];
-	}
-	
-	public function getBerichten()
-	{
-		$response =  $this->client->request('GET', '/sjablonen', [
-				'query' => ['type' => 'bericht']
-		]);
-		
-		$response = json_decode($response->getBody()->getContents(), true);
-		return $response["hydra:member"];
-	}
-	
-	public function getSlug($slug)
+	public function getOnSlug($slug)
 	{
 		
-		$response =  $this->client->request('GET', '/sjablonen', [
-				'query' => ['slug' => $slug]
-		]);
+		$response =  $this->client->request('GET', $slug);
 		
 		$response = json_decode($response->getBody()->getContents(), true);
-		return $response["hydra:member"];
-	}
-	
-	public function getOne($id)
-	{
-		$response = $this->client->request('GET','/sjablonen/'.$id);
-		$response = json_decode($response->getBody()->getContents(), true);
-		return $response;
-	}
-	
-	public function render($id, $variabelen)
-	{
-		$response = $this->client->post('/sjablonen/'.$id.'/render', [
-				\GuzzleHttp\RequestOptions::JSON => $variabelen
-		]);
-		$response = json_decode($response->getBody()->getContents(), true);
-		return $response;
-	}
-	
-	
-	public function save($sjabloon)
-	{
-		unset($sjabloon['bericht']);
-		unset($sjabloon['pagina']);
-		
-		if($sjabloon['id']){
-			$response = $this->client->put('sjablonen/'.$sjabloon['id'], [
-					\GuzzleHttp\RequestOptions::JSON => $sjabloon
-			]);
-		}
-		else{
-			unset($sjabloon['id']);
-			$response = $this->client->post('sjablonen', [
-					\GuzzleHttp\RequestOptions::JSON => $sjabloon
-			]);
-		}
-		
-		//$response=  $this->client->send($request);
-		$response = json_decode($response->getBody()->getContents(), true);
-		return $response;
+		return $response["template"]["content"];
 	}
 	
 }
